6266cd46734db9fa06fe63cd17fd355935b23493
mcarrasco <maria.carrasco@zalando.de>
2018-06-25 14:43:14 +0200

    Update apollo-codegen to latest version 0.20.0

--- types/apollo-codegen/index.d.ts
+++ types/apollo-codegen/index.d.ts
@@ -1,40 +1,39 @@
-// Type definitions for apollo-codegen 0.16
+// Type definitions for apollo-codegen 0.20.0
 // Project: https://github.com/apollographql/apollo-codegen
-// Definitions by: Bradley Ayers <https://github.com/bradleyayers>
+// Definitions by: Bradley Ayers <https://github.com/bradleyayers>, Maria Carrasco <https://github.com/kostspielig>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
+// TypeScript Version: 2.9.2
+
+import { Options } from 'graphql/utilities/schemaPrinter';
 
 export function downloadSchema(
-    url: string,
-    outputPath: string,
-    additionalHeaders: { [name: string]: string },
-    insecure: boolean,
-    method: string
+  url: string,
+  outputPath: string,
+  additionalHeaders: { [name: string]: string; },
+  insecure: boolean,
+  method: string
 ): Promise<void>;
 
 export function introspectSchema(
-    schemaPath: string,
-    outputPath: string
+  schemaPath: string,
+  outputPath: string
 ): Promise<void>;
 
 export function printSchema(
-    schemaPath: string,
-    outputPath: string
+  schemaPath: string,
+  outputPath: string,
+  options?: Options
 ): Promise<void>;
 
+declare type TargetType = 'json' | 'swift' | 'ts-legacy' | 'typescript-legacy' | 'flow-legacy' | 'scala' | 'flow' | 'typescript' | 'ts';
+
 export function generate(
-    inputPaths: string[],
-    schemaPath: string,
-    outputPath: string,
-    target: "json" | "swift" | "ts" | "typescript" | "flow" | "scala",
-    tagName: string,
-    options: {
-        passthroughCustomScalars: boolean;
-        customScalarsPrefix: string;
-        addTypename: boolean;
-        namespace: string;
-        operationIdsPath: string | null;
-        generateOperationIds: boolean;
-        mergeInFieldsFromFragmentSpreads: boolean;
-    }
+  inputPaths: string[],
+  schemaPath: string,
+  outputPath: string,
+  only: string,
+  target: TargetType,
+  tagName: string,
+  projectName: string,
+  options: any
 ): void;
